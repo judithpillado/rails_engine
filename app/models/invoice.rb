d3095bc18001b799ff28304e84c6bd18b14b68b9
Judith Pillado <judith.pillado@gmail.com>
2020-10-20 18:26:28 -0500

    Create Invoice model with corresponding test

---
+++ app/models/invoice.rb
@@ -0,0 +1,8 @@
+class Invoice < ApplicationRecord
+  validates :status, presence: true
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :transactions
+end
