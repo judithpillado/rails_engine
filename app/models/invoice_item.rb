4ee8278926f50037dd8185adb6e2e5e8ed7ffe73
Judith Pillado <judith.pillado@gmail.com>
2020-10-20 23:32:26 -0500

    Create method pennies to dollars

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,8 @@ class InvoiceItem < ApplicationRecord
   validates :unit_price, presence: true
   belongs_to :item
   belongs_to :invoice
+
+  def pennies_to_dollars
+    unit_price / 100.0
+  end
 end
